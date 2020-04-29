3e6935e0e4654be73497811849cd43712dd0cc95
gwmccubbin <gwmccubbin@gmail.com>
2020-04-29 13:11:59 -0500

    WIP Adds smart contract

---
+++ src/contracts/Asset.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.5.0;
+
+contract Asset {
+  string public name;
+  address public custodian;
+  States public state;
+
+  enum States {
+    Created,
+    Sent,
+    Received,
+  }
+
+  event Action(
+    string name,
+    string account,
+    string custodian,
+    uint timestamp
+  );
+
+  constructor(string memory _name) public {
+    name = _name;
+    custodian = msg.sender;
+    state = States.Created;
+    emit Action("CREATE", msg.sender, msg.sender, now)
+  }
+
+  function send(address _to) public {
+    // Must be custodian to send
+    require(msg.sender == custodian);
+
+    // Can't be in "Sent" state
+    // Must be "Created" or "Received"
+    require(state != States.Sent);
+
+    // Update state to "Sent"
+    state = States.Sent;
+
+    // Make _to new custodian
+    custodian = _to;
+
+    // Log history
+    emit Action("SEND", msg.sender, _to, now);
+  }
+
+  function receive() public {
+    // Must be custodian to receive
+    require(msg.sender == custodian);
+
+    // Must be in "Sent" state
+    // Cannot be "Created" or "Received"
+    require(state == States.Sent);
+
+    // Update state to "Received"
+    state = States.Received;
+
+    // Log history
+    emit Action("RECEIVE", msg.sender, msg.sender, now);
+  }
+}
