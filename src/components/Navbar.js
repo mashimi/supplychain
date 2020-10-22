6a975f6a7ca87e0c66a481d5b5d260da1a8e3281
mashimi <lmashimi@gmail.com>
2020-10-22 13:30:01 +0200

    change network

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,11 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="http://blockchainmaster.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dapp University Supply Chain
+          Mashimi Supply Chain
         </a>
 
         <ul className="navbar-nav px-3">
--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,18 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 4
+    }
+
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/artifacts/',
